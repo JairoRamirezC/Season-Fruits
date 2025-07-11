1a191fa6f6893aad1e19cde2cbdc479bfd9f871c
Jairo Ramirez <jairoramirez_15@hotmail.com>
2025-07-11 12:31:18 -0500

    feat: file eslint.config.js modified for work with react project

--- eslint.config.js
+++ eslint.config.js
@@ -2,34 +2,44 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
+import pluginReactHooks from "eslint-plugin-react-hooks";
 import { defineConfig } from "eslint/config";
 
 
 export default defineConfig([
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
   { 
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], 
+    files: ["**/*.{js,jsx,ts,tsx}"],
     plugins: { 
-      js,
-      react: pluginReact 
-    }, 
-    extends: [
-      "js/recommended",
-      "plugin:react/recommended",
-      "plugin:react/jsx-runtime"
-    ],
+      'react': pluginReact,
+      'react-hooks': pluginReactHooks
+    },
     rules: {
       "react/react-in-jsx-scope": "off", // Desactiva la necesidad de importar React
-      "react/jsx-uses-react": "off",      // Desactiva la verificación de uso de React
-      '@typescript-eslint/explicit-function-return-type': 'off',
-      '@typescript-eslint/no-unused-vars': 'warn',
+      "react/jsx-uses-react": "off", // Desactiva la verificación de uso de React
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+      "react/display-name": "off"
     },
-  },
-  { 
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], 
-    languageOptions: { 
-      globals: globals.browser 
-    } 
-  },
-  tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.es2021
+      },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+        }
+      }
+    },
+    settings: {
+      react: {
+        version: "detect"
+      }
+    }
+  }
 ]);
